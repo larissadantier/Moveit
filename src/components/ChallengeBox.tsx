32270aebf5cd5914720f6e0653f34c8de7196224
larissadantier <larissa_dantier@hotmail.com>
2021-04-07 22:46:04 -0300

    Prettier - ChallengeBox

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,39 +1,32 @@
 import styles from "../styles/components/ChallengeBox.module.css";
 
 export function ChallengeBox() {
-    const hasActiveChallenge = true;
-
+  const hasActiveChallenge = true;
 
   return (
     <div className={styles.challengeBoxContainer}>
       {hasActiveChallenge ? (
-          <div className={styles.challengeActive}>
-              <header>Ganhe 400px</header>
+        <div className={styles.challengeActive}>
+          <header>Ganhe 400px</header>
 
-              <main className={styles.newChallenger}>
-                  <img src="icons/body.svg" />
-                  <strong>Novo desafio</strong>
-                  <p>Levante e faça uma caminhada de 3 minutos</p>
-              </main>
+          <main className={styles.newChallenger}>
+            <img src="icons/body.svg" />
+            <strong>Novo desafio</strong>
+            <p>Levante e faça uma caminhada de 3 minutos</p>
+          </main>
 
-              <footer>
-                  <button 
-                  type='button'
-                  className={styles.challengeFailedButton}
-                  >
-                      Falhei
-                  </button>
+          <footer>
+            <button type="button" className={styles.challengeFailedButton}>
+              Falhei
+            </button>
 
-                  <button 
-                  type='button'
-                  className={styles.challengeSucessButton}
-                  >
-                      Completei
-                  </button>
-              </footer>
-          </div>
+            <button type="button" className={styles.challengeSucessButton}>
+              Completei
+            </button>
+          </footer>
+        </div>
       ) : (
-          <div className={styles.challengeNotActive}>
+        <div className={styles.challengeNotActive}>
           <strong>Finalize um ciclo para receber um desafio</strong>
           <p>
             <img src="icons/level-up.svg" alt="Level Up" />
